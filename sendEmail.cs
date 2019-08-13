241cad343cb128c63a86ec29b7069fa905c29f64
mab5113 <bradbury5113@outlook.com>
2019-08-12 20:38:58 -0500

    Update sendEmail.cs

--- sendEmail.cs
+++ sendEmail.cs
@@ -1,26 +1,40 @@
 ﻿//Code to send an email through a c# application
- private void eMailMessage()
-        {
+using System;
+namespace sendEmail
+{
+	public class emailMessage
+	{
+		private void main()
+		{
+			string messageTo = "";
+			string messageBody = "";
+			string messageSubject = "";
 
-        Console.WriteLine("What do you want the message to say?");
-        Console.ReadLine();
-            try
-            {
-                Microsoft.Office.Interop.Outlook.Application objOutlook = new Microsoft.Office.Interop.Outlook.Application();
+			Console.WriteLine("Who do you want to send the email to?");
+			messageTo = Console.ReadLine();
+			Console.WriteLine = ("What is the subject of the email?");
+			messageSubject = Console.ReadLine();
+			Console.WriteLine("What do you want the message to say?");
+			messageBody = Console.ReadLine();
+			try
+			{
+				Microsoft.Office.Interop.Outlook.Application objOutlook = new Microsoft.Office.Interop.Outlook.Application();
 
-                Microsoft.Office.Interop.Outlook.MailItem mailItem = objOutlook.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem)
-                    as Microsoft.Office.Interop.Outlook.MailItem;
+				Microsoft.Office.Interop.Outlook.MailItem mailItem = objOutlook.CreateItem(Microsoft.Office.Interop.Outlook.OlItemType.olMailItem)
+					as Microsoft.Office.Interop.Outlook.MailItem;
 
-                mailItem.Subject = "New application needs approval";
-                mailItem.To = "mattlog21@gmail.com";
-                mailItem.Body = "Testing this out";
-                mailItem.Importance = Microsoft.Office.Interop.Outlook.OlImportance.olImportanceLow;
-                mailItem.Display(false);
-                ((Microsoft.Office.Interop.Outlook._MailItem)mailItem).Send();
-                MessageBox.Show("Email Sent!");
-            }
-            catch
-            {
-                MessageBox.Show("The email could not be sent!");
-            }
-        }
+				mailItem.Subject = messageSubject;
+				mailItem.To = messageTo;
+				mailItem.Body = messageBody;
+				mailItem.Importance = Microsoft.Office.Interop.Outlook.OlImportance.olImportanceLow;
+				mailItem.Display(false);
+				((Microsoft.Office.Interop.Outlook._MailItem)mailItem).Send();
+				Console.WriteLine("Email Sent!");
+			}
+			catch
+			{
+				MessageBox.Show("The email could not be sent!");
+			}
+		}
+	}
+}
